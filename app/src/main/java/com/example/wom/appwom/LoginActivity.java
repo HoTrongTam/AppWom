5766f34e98082a8cf7fc86f09f3c6b21da7102db
DESKTOP-RMH3DM1\hotrongtam <hotrongtam0207@gmail.com>
2017-05-27 12:01:18 +0700

    extens fragment

--- app/src/main/java/com/example/wom/appwom/LoginActivity.java
+++ app/src/main/java/com/example/wom/appwom/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.example.wom.appwom;
 
+import android.app.Fragment;
+
 /**
  * Created by hotrongtam on 5/27/2017.
  */
 
-public class LoginActivity {
+public class LoginActivity extends Fragment{
 }
