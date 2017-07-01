23ae00a2264addf8659be9862855f42512b3fd2e
Duyên Nguyễn Minh <duyennmps04044@gmail.com>
2017-07-01 22:27:06 +0700

    Cập nhật đăng ký

--- app/src/main/java/com/example/wom/appwom/DBHelper/APIConfig.java
+++ app/src/main/java/com/example/wom/appwom/DBHelper/APIConfig.java
@@ -1,11 +1,14 @@
 package com.example.wom.appwom.DBHelper;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
 /**
  * Created by Eagle007 on 30/06/2017.
  */
 
 public class APIConfig {
-    public static final String locahost = "192.168.39.107";
+    public static final String locahost = "192.168.1.9";
     public static final String URL_Login = "http://"+locahost+":8081/WOM/getTaiKhoan.php";
     public static final String URL_Register = "http://"+locahost+":8081/WOM/postTaiKhoan.php";
     public static final String URL_LoadProduct = "";
@@ -15,4 +18,10 @@ public class APIConfig {
     public static final String QUANGCAO_02 = "http://znews-photo-td.zadn.vn/w480/Uploaded/OFH_oazszstq/2017_05_18/cothemthongtinveiphone801.jpg";
     public static final String QUANGCAO_03 = "https://cdn.mediamart.vn/News/mua-vaio-rinh-qua-xperia-cung-media-mart-924201273231am.jpg";
     public static final String QUANGCAO_04 = "http://cdn.images.express.co.uk/img/dynamic/59/590x/blackberry-keyone-uk-price-release-date-mwc-2017-772697.jpg";
+
+    // Random kiểu chữ và số
+    SecureRandom random = new SecureRandom();
+    public String randomChu() {
+        return new BigInteger(28, random).toString(32).toUpperCase();
+    }
 }
--- app/src/main/java/com/example/wom/appwom/DangKyActivity.java
+++ app/src/main/java/com/example/wom/appwom/DangKyActivity.java
@@ -2,13 +2,46 @@ package com.example.wom.appwom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.wom.appwom.DBHelper.APIConfig;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class DangKyActivity extends AppCompatActivity {
-
-
+    @BindView(R.id.btnDangKy)
+    Button btnDangKy;
+    @BindView(R.id.btnNhapLaiDK)
+    Button btnNhapLaiDK;
+    @BindView(R.id.edtEmailDK)
+    EditText edtEmailDK;
+    @BindView(R.id.txtMaXacNhanDK)
+    TextView txtMaXacNhan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_ky);
+        // Khởi tạo butter knife
+        ButterKnife.bind(this);
+    }
+    @OnClick({R.id.btnDangKy,R.id.btnNhapLaiDK})
+    public void btnDangKy(Button button) {
+        if (button.getId() == R.id.btnDangKy)
+        edtEmailDK.setText("AAA"); txtMaXacNhan.setText(new APIConfig().randomChu());
+    }
+    /* Toast ở trang Đăng Ký*/
+    private void Toast(String toast){
+        Toast.makeText(DangKyActivity.this, toast, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        txtMaXacNhan.setText(new APIConfig().randomChu());
+        super.onStart();
     }
 }
