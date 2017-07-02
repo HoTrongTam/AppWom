c84a38021ecc8648f449700d96a52ee60af0cdc3
Duyên Nguyễn Minh <duyennmps04044@gmail.com>
2017-07-02 14:17:03 +0700

    NavigationView

--- app/src/main/java/com/example/wom/appwom/HomeActivity.java
+++ app/src/main/java/com/example/wom/appwom/HomeActivity.java
@@ -226,6 +226,8 @@ public class HomeActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
+            Intent intent = new Intent(HomeActivity.this, ThongTinTaiKhoanActivity.class);
+            startActivity(intent);
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
