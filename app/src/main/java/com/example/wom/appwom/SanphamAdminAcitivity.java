300f4e5363e76f0d8f68bdf4c1b401107927e4eb
Quang <quangprobt@gmail.com>
2017-07-07 00:46:11 +0700

    fix Giỏ hàng + thêm sản phẩm

--- app/src/main/java/com/example/wom/appwom/SanphamAdminAcitivity.java
+++ app/src/main/java/com/example/wom/appwom/SanphamAdminAcitivity.java
@@ -107,4 +107,5 @@ public class SanphamAdminAcitivity extends AppCompatActivity {
             }
         });
     }
+
 }
--- app/src/main/java/com/example/wom/appwom/ThemmoiSanphamAdmin.java
+++ app/src/main/java/com/example/wom/appwom/ThemmoiSanphamAdmin.java
@@ -1,13 +1,11 @@
 package com.example.wom.appwom;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -82,14 +80,13 @@ public class ThemmoiSanphamAdmin extends AppCompatActivity {
                         }
                     };
                     requestQueue.add(stringRequest);
-                    Toast.makeText(ThemmoiSanphamAdmin.this,"Thêm thành công",Toast.LENGTH_LONG).show();
-                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                    startActivity(intent);
                 }else{
                     CheckConnection.ShowToast_Short(getApplicationContext(),"Kiểm tra lại dữ liệu");
                 }
+
             }
         });
+
     }
 
     private void Anhxa() {
