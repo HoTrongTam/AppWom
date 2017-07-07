b467df3db8f7f70208100e948e813a65184451f3
Duyên Nguyễn Minh <duyennmps04044@gmail.com>
2017-07-07 19:32:54 +0700

    Fix Thanh toán đơn hàng

--- app/src/main/java/com/example/wom/appwom/DBHelper/APIConfig.java
+++ app/src/main/java/com/example/wom/appwom/DBHelper/APIConfig.java
@@ -8,7 +8,7 @@ import java.security.SecureRandom;
  */
 
 public class APIConfig {
-    public static final String locahost = "192.168.1.100";
+    public static final String locahost = "10.0.45.35";
     public static final String URL_Login = "http://"+locahost+":8081/WOM/getTaiKhoan.php";
     public static final String URL_getTaikhoan = "http://"+locahost+":8081/WOM/getTaiKhoan2.php";
     public static final String URL_getThongTinTaiKhoan = "http://"+locahost+":8081/WOM/getThongTinTaiKhoan.php";
--- app/src/main/java/com/example/wom/appwom/GiohangActivity.java
+++ app/src/main/java/com/example/wom/appwom/GiohangActivity.java
@@ -1,8 +1,10 @@
 package com.example.wom.appwom;
 
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -13,8 +15,20 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.wom.appwom.Adapter.GioHangAdapter;
+import com.example.wom.appwom.DBHelper.APIConfig;
+import com.example.wom.appwom.Model.Giohang;
 import com.example.wom.appwom.Util.CheckConnection;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.example.wom.appwom.DBHelper.APIConfig.USER_LOGIN_ID;
 
@@ -23,8 +37,10 @@ public class GiohangActivity extends AppCompatActivity {
     TextView txtThongbao;
     static TextView txtTongtien;
     Button btnThanhToan, btnTieptuc;
-    Toolbar  toolbar;
+    Toolbar toolbar;
     GioHangAdapter gioHangAdapter;
+    ProgressDialog mProgress;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,7 +50,12 @@ public class GiohangActivity extends AppCompatActivity {
         EventUltil();
         EventButton();
         CatchOnItemListView();
-
+                /* Progress */
+        mProgress = new ProgressDialog(this);
+        String titleId = "Đang thực hiện giao dịch...";
+        mProgress.setTitle(titleId);
+        mProgress.setCancelable(false);
+        mProgress.setMessage("Vui lòng chờ trong giây lát...");
     }
 
     private void CatchOnItemListView() {
@@ -47,15 +68,15 @@ public class GiohangActivity extends AppCompatActivity {
                 builder.setPositiveButton("Có", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int i) {
-                        if(HomeActivity.mangGiohang.size() <= 0){
+                        if (HomeActivity.mangGiohang.size() <= 0) {
                             txtThongbao.setVisibility(View.VISIBLE);
-                        }else{
+                        } else {
                             HomeActivity.mangGiohang.remove(position);
                             gioHangAdapter.notifyDataSetChanged();
                             EventUltil();
-                            if(HomeActivity.mangGiohang.size()<=0){
+                            if (HomeActivity.mangGiohang.size() <= 0) {
                                 txtThongbao.setVisibility(View.VISIBLE);
-                            }else{
+                            } else {
                                 txtThongbao.setVisibility(View.INVISIBLE);
                                 gioHangAdapter.notifyDataSetChanged();
                                 EventUltil();
@@ -80,29 +101,45 @@ public class GiohangActivity extends AppCompatActivity {
         btnTieptuc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
                 startActivity(intent);
             }
         });
         btnThanhToan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(HomeActivity.mangGiohang.size()>0){
+                if (HomeActivity.mangGiohang.size() > 0) {
                     int id;
                     long gia;
                     int sl;
                     String ten;
-                    for(int i = 0; i < HomeActivity.mangGiohang.size();i++){
+                    for (int i = 0; i < HomeActivity.mangGiohang.size(); i++) {
                         id = HomeActivity.mangGiohang.get(i).getIdsp();
                         gia = HomeActivity.mangGiohang.get(i).getGiasp();
                         sl = HomeActivity.mangGiohang.get(i).getSoluongsp();
                         ten = HomeActivity.mangGiohang.get(i).getTensp();
-                        txtTongtien.setText(USER_LOGIN_ID);
                     }
-                    Toast.makeText(getApplicationContext(),HomeActivity.mangGiohang.toString(),Toast.LENGTH_LONG);
-                }else{
+                    POST_DONHANG();
+                    // bắt sự kiện khi đã bắt lỗi thành công, sau đó thực hiện sửa thông tin
+                    mProgress.show();
+
+                    Runnable progressRunnable = new Runnable() {
+                        @Override
+                        public void run() {
+                            // xác nhận post
+                            Toast.makeText(GiohangActivity.this, "Thanh toán thành công", Toast.LENGTH_SHORT).show();
+                            mProgress.cancel();
+                            HomeActivity.mangGiohang.clear();
+                            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                            startActivity(intent);
+                        }
+                    };
+
+                    Handler pdCanceller = new Handler();
+                    pdCanceller.postDelayed(progressRunnable, 2000);
+                } else {
                     CheckConnection.ShowToast_Short
-                            (getApplicationContext(),"Giỏ hàng của bạn chưa có sản phẩm để thanh toán");
+                            (getApplicationContext(), "Giỏ hàng của bạn chưa có sản phẩm để thanh toán");
                 }
             }
         });
@@ -110,33 +147,90 @@ public class GiohangActivity extends AppCompatActivity {
 
     public static void EventUltil() {
         long tongtien = 0;
-        for(int i = 0; i < HomeActivity.mangGiohang.size();i++){
+        for (int i = 0; i < HomeActivity.mangGiohang.size(); i++) {
             tongtien += HomeActivity.mangGiohang.get(i).getGiasp();
         }
         txtTongtien.setText(tongtien + " VNĐ");
     }
 
     private void CheckData() {
-        if(HomeActivity.mangGiohang.size() <=0){
+        if (HomeActivity.mangGiohang.size() <= 0) {
             lvGiohang.deferNotifyDataSetChanged();
             txtThongbao.setVisibility(View.VISIBLE);
             lvGiohang.setVisibility(View.INVISIBLE);
-        }else{
+        } else {
             lvGiohang.deferNotifyDataSetChanged();
             txtThongbao.setVisibility(View.INVISIBLE);
             lvGiohang.setVisibility(View.VISIBLE);
         }
     }
 
-    public void Anhxa(){
-        lvGiohang = (ListView)findViewById(R.id.listViewGiohang);
-        txtThongbao = (TextView)findViewById(R.id.textViewThongbao);
-        txtTongtien = (TextView)findViewById(R.id.textViewTongtien);
-        btnThanhToan = (Button)findViewById(R.id.ButtonThanhtoan);
-        btnTieptuc = (Button)findViewById(R.id.ButtonTieptucmua);
-        gioHangAdapter = new GioHangAdapter( GiohangActivity.this,HomeActivity.mangGiohang);
+    public void Anhxa() {
+        lvGiohang = (ListView) findViewById(R.id.listViewGiohang);
+        txtThongbao = (TextView) findViewById(R.id.textViewThongbao);
+        txtTongtien = (TextView) findViewById(R.id.textViewTongtien);
+        btnThanhToan = (Button) findViewById(R.id.ButtonThanhtoan);
+        btnTieptuc = (Button) findViewById(R.id.ButtonTieptucmua);
+        gioHangAdapter = new GioHangAdapter(GiohangActivity.this, HomeActivity.mangGiohang);
         lvGiohang.setAdapter(gioHangAdapter);
 
+    }
+
+    // Xác nhận mua hàng
+    String DONHANG_ID;
+
+    private void POST_DONHANG() {
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, APIConfig.URL_insertDonHang, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //Toast.makeText(GiohangActivity.this, ""+response, Toast.LENGTH_SHORT).show(); // trả về id đơn hàng
+                DONHANG_ID = response;
+                for (int i = 0; i < HomeActivity.mangGiohang.size(); i++) {
+                    Giohang giohang = HomeActivity.mangGiohang.get(i);
+                    POST_CHITIETDONHANG("" + giohang.getIdsp(), "" + giohang.getSoluongsp());
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String, String> hashMap = new HashMap<String, String>();
+                hashMap.put("id_tk", USER_LOGIN_ID);
+                hashMap.put("tinhtrangdh", "0");
+                return hashMap;
+            }
+        };
+        requestQueue.add(stringRequest);
+    }
+
+    private void POST_CHITIETDONHANG(final String id_sanpham, final String soluong) {
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, APIConfig.URL_insertChiTietDonHang, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //   Toast.makeText(GiohangActivity.this, ""+response, Toast.LENGTH_SHORT).show(); // trả về id chi tiet đơn hàng
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String, String> hashMap = new HashMap<String, String>();
+                hashMap.put("id_sanpham", id_sanpham);
+                hashMap.put("id_donhang", DONHANG_ID);
+                hashMap.put("soluong", soluong);
+                return hashMap;
+            }
+        };
+        requestQueue.add(stringRequest);
     }
 
 }
